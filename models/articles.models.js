0fadf0240e19a460f69717e31f94b1357a517326
TheStringKing <dandreopperman@gmail.com>
2024-08-27 12:34:36 +0100

    add some refactoring to tests for the /api endpoint

--- models/articles.models.js
+++ models/articles.models.js
@@ -3,7 +3,6 @@ exports.selectArticleById = (article_id) => {
     return db
       .query('SELECT * FROM articles WHERE article_id = $1;', [article_id])
       .then(({rows}) => {
-        console.log(rows)
         if(rows.length===0){return Promise.reject({ message: "article does not exist", status: 404})}
         return rows[0];
       });
