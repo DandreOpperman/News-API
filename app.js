856b5c1a43f2764c492298f3676bbb943a38bb8b
TheStringKing <dandreopperman@gmail.com>
2024-08-27 12:03:14 +0100

    4-get-topics-by-topic_id, this will add tests to app.test.js, two new files, one in controllers and one in models for the topics by topic_id endpoint, which has been added to the app.js file

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ const express = require('express');
 const app = express();
 const{getTopics} = require('./controllers/topics.controllers')
 const {getEndPoints}=require('./controllers/api.controller')
-// const {getTopics, getEndPoints}=require('./controllers/index')
-const {serverErrorHandler} =require('./error')
+const {getArticleById}=require('./controllers/articles.controllers')
+const {serverErrorHandler, customErrorHandler, idErrorHandler} =require('./error')
 
 app.get('/api/topics', getTopics)
 app.get('/api', getEndPoints)
+app.get('/api/articles/:article_id', getArticleById)
+app.use(customErrorHandler)
+app.use(idErrorHandler)
 app.all('/*', (req, res)=>{
     res.status(404).send({message : 'Route not found'})
 })
